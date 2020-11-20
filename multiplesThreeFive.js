33150888a7d539033a386dbf9d993138210574a2
blangwell <b.langwell@outlook.com>
2020-11-19 18:37:09 -0800

    [multiples] Add refactored solution function

--- multiplesThreeFive.js
+++ multiplesThreeFive.js
@@ -1,11 +1,17 @@
 // natural numbers below 10 that are multiples of 3 and 5 => 3, 5, 6, 9 => sum 23
 // find the sum of all multiples of 3 and 5 below argument
 
-function sumMult(cutoff) {
-  let arr = [...Array(cutoff).keys()]
-  arr = arr.filter(a => a % 3 === 0 || a % 5 === 0);
-  console.log(arr)
-  return arr.reduce((acc, curr) => acc + curr)
-}
+// function sumMult(cutoff) {
+//   let arr = [...Array(cutoff).keys()].filter(a => a % 3 === 0 || a % 5 === 0)
+//   return arr.reduce((acc, curr) => acc + curr)
+// }
 
-console.log(sumMult(10))
+// console.log(sumMult(30))
+
+
+// submitted answer with edge cases
+function solution(number){
+  if (number < 0 || !number) return 0;
+  let multiples = [...Array(number).keys()].filter(a => a % 3 === 0 || a % 5 === 0);
+  return multiples.reduce((acc, curr) => acc + curr); 
+}
